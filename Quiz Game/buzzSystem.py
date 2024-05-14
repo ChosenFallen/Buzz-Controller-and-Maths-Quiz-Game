1e4f554110e1248f772b691484d24c84c8bd1d26
Curtis <curtis.stewart722@gmail.com>
2024-05-14 12:46:00 +0100

    sourcery changes

--- Quiz Game/buzzSystem.py
+++ Quiz Game/buzzSystem.py
@@ -1,10 +1,10 @@
 from enum import Enum, auto
+from random import choice
 from threading import Thread
 
 import usb.backend.libusb1  # type: ignore
 import usb.core  # type: ignore
 import usb.util  # type: ignore
-from random import choice
 
 
 class Colours(Enum):
@@ -24,9 +24,7 @@ class BuzzBrain:
         self.interface = 0
 
         self.controllers: list[BuzzController] = []
-        for i in range(4):
-            self.controllers.append(BuzzController(brain=self, index=i))
-
+        self.controllers.extend(BuzzController(brain=self, index=i) for i in range(4))
         self.device.set_configuration()  # type: ignore
         usb.util.claim_interface(self.device, self.interface)
         cfg = self.device.get_active_configuration()  # type: ignore
@@ -65,37 +63,58 @@ class BuzzBrain:
         self.thread = None
 
     def parse_controller(self, data: bytes) -> None:
-        # Controller 1
+        button_masks = [
+            (1, 2, 4, 8, 16),
+            (32, 64, 128, 1, 2),
+            (4, 8, 16, 32, 64),
+            (128, 1, 2, 4, 8),
+        ]
+        data_index_arrays = [
+            (2, 2, 2, 2, 2),
+            (2, 2, 2, 3, 3),
+            (3, 3, 3, 3, 3),
+            (3, 4, 4, 4, 4),
+        ]
+        for i, controller in enumerate(self.controllers):
+            mask = button_masks[i]
+            data_indexes = data_index_arrays[i]
+            controller.Red = bool(data[data_indexes[0]] & mask[0])
+            controller.Yellow = bool(data[data_indexes[1]] & mask[1])
+            controller.Green = bool(data[data_indexes[2]] & mask[2])
+            controller.Orange = bool(data[data_indexes[3]] & mask[3])
+            controller.Blue = bool(data[data_indexes[4]] & mask[4])
 
-        self.controllers[0].Red = True if data[2] & 1 else False
-        self.controllers[0].Yellow = True if data[2] & 2 else False
-        self.controllers[0].Green = True if data[2] & 4 else False
-        self.controllers[0].Orange = True if data[2] & 8 else False
-        self.controllers[0].Blue = True if data[2] & 16 else False
+        # # Controller 1
 
-        # Controller 2
+        # self.controllers[0].Red = True if data[2] & 1 else False
+        # self.controllers[0].Yellow = True if data[2] & 2 else False
+        # self.controllers[0].Green = True if data[2] & 4 else False
+        # self.controllers[0].Orange = True if data[2] & 8 else False
+        # self.controllers[0].Blue = True if data[2] & 16 else False
 
-        self.controllers[1].Red = True if data[2] & 32 else False
-        self.controllers[1].Yellow = True if data[2] & 64 else False
-        self.controllers[1].Green = True if data[2] & 128 else False
-        self.controllers[1].Orange = True if data[3] & 1 else False
-        self.controllers[1].Blue = True if data[3] & 2 else False
+        # # Controller 2
 
-        # Controller 3
+        # self.controllers[1].Red = True if data[2] & 32 else False
+        # self.controllers[1].Yellow = True if data[2] & 64 else False
+        # self.controllers[1].Green = True if data[2] & 128 else False
+        # self.controllers[1].Orange = True if data[3] & 1 else False
+        # self.controllers[1].Blue = True if data[3] & 2 else False
 
-        self.controllers[2].Red = True if data[3] & 4 else False
-        self.controllers[2].Yellow = True if data[3] & 8 else False
-        self.controllers[2].Green = True if data[3] & 16 else False
-        self.controllers[2].Orange = True if data[3] & 32 else False
-        self.controllers[2].Blue = True if data[3] & 64 else False
+        # # Controller 3
 
-        # Controller 4
+        # self.controllers[2].Red = True if data[3] & 4 else False
+        # self.controllers[2].Yellow = True if data[3] & 8 else False
+        # self.controllers[2].Green = True if data[3] & 16 else False
+        # self.controllers[2].Orange = True if data[3] & 32 else False
+        # self.controllers[2].Blue = True if data[3] & 64 else False
 
-        self.controllers[3].Red = True if data[3] & 128 else False
-        self.controllers[3].Yellow = True if data[4] & 1 else False
-        self.controllers[3].Green = True if data[4] & 2 else False
-        self.controllers[3].Orange = True if data[4] & 4 else False
-        self.controllers[3].Blue = True if data[4] & 8 else False
+        # # Controller 4
+
+        # self.controllers[3].Red = True if data[3] & 128 else False
+        # self.controllers[3].Yellow = True if data[4] & 1 else False
+        # self.controllers[3].Green = True if data[4] & 2 else False
+        # self.controllers[3].Orange = True if data[4] & 4 else False
+        # self.controllers[3].Blue = True if data[4] & 8 else False
 
         oldbits = self.bits
         self.bits = (data[4] << 16) + (data[3] << 8) + data[2]
@@ -125,7 +144,7 @@ class BuzzController:
         self.Light: bool = False
 
     def is_buttons_pressed(self, list_of_buttons: list[str]) -> bool:
-        return all([self.is_button_pressed(button) for button in list_of_buttons])
+        return all(self.is_button_pressed(button) for button in list_of_buttons)
 
     def get_buttons_states(self) -> dict[str, bool]:
         return {
@@ -144,17 +163,28 @@ class BuzzController:
     #     self.Blue = buttons["blue"]
 
     def is_button_pressed(self, button: str) -> bool:
-        return self.__dict__[button.title()]
+        button_state = {
+            "red": self.Red,
+            "yellow": self.Yellow,
+            "green": self.Green,
+            "orange": self.Orange,
+            "blue": self.Blue,
+        }
+        return button_state[button.lower()]
 
 
 class QuizBuzzBrain(BuzzBrain):
-    def first_pressed_controller(self, in_play_controllers: list[int] = [0, 1, 2, 3]) -> None | int:
-        # Return the index of the first pressed controller
-        pressed = []
-        for controller in self.controllers:
-            if controller.index in in_play_controllers and controller.is_buttons_pressed(["yellow", "green", "orange", "blue"]):
-                pressed.append(self.controllers.index(controller))
-                
+    def first_pressed_controller(
+        self, in_play_controllers: list[int] | None = None
+    ) -> None | int:
+        if in_play_controllers is None:
+            in_play_controllers = [0, 1, 2, 3]
+        pressed = [
+            idx
+            for idx, controller in enumerate(self.controllers)
+            if controller.index in in_play_controllers
+            and controller.is_buttons_pressed(["yellow", "green", "orange", "blue"])
+        ]
         match len(pressed):
             case 0:
                 return None
@@ -164,11 +194,11 @@ class QuizBuzzBrain(BuzzBrain):
                 return choice(pressed)
 
     def first_pressed_button(self, controller_index: int) -> str:
-        pressed = []
-        for colour in ["yellow", "green", "orange", "blue"]:
-            if self.controllers[controller_index].is_button_pressed(colour):
-                pressed.append(colour)
-        
+        pressed = [
+            colour
+            for colour in ["yellow", "green", "orange", "blue"]
+            if self.controllers[controller_index].is_button_pressed(colour)
+        ]
         match len(pressed):
             case 1:
                 return pressed[0]
