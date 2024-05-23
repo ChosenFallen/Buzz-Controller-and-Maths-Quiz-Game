f5b18ac5b72ee5394f5b9298e9b54ba8f76a81eb
Curtis Stewart <curtis.stewart722@gmail.com>
2024-05-23 15:26:54 +0100

    separate the back button class

--- menu.py
+++ menu.py
@@ -64,16 +64,15 @@ class SettingsMenu(BaseState):
         # self.menu_group = pygame.sprite.Group()  # type: ignore
 
         self.title = Title(
-            [all_sprites, self.state_group],
-            title_font,
-            "Settings",
-            (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4),
+            groups=[all_sprites, self.state_group],
+            font=title_font,
+            text="Settings",
+            pos=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4),
         )
         self.go_back_button = BackButton(
-            [all_sprites, buttons_group, self.state_group],
-            (WINDOW_WIDTH / 16, WINDOW_HEIGHT / 8),
-            arrow_font,
-            self.go_back,
+            groups=[all_sprites, buttons_group, self.state_group],
+            pos=(WINDOW_WIDTH / 16, WINDOW_HEIGHT / 8),
+            function=self.go_back,
             size=(50, 50),
         )
         # self.start_button = Button(
@@ -132,6 +131,7 @@ class Button(pygame.sprite.Sprite):
 
         self.image = self.normal_surf
         self.rect: pygame.FRect = self.image.get_frect(center=pos)  # type: ignore
+        self.normal_rect: pygame.FRect = self.normal_surf.get_frect(center=pos)
 
     def create_normal_surf(self) -> pygame.Surface:
         surf = pygame.Surface(
@@ -221,14 +221,29 @@ class Button(pygame.sprite.Sprite):
 
     def isOver(self, pos):
         # Pos is the mouse position or a tuple of (x, y) coordinates
-        return self.rect.collidepoint(pos)
+        return self.normal_rect.collidepoint(pos)
 
 
-class BackButton(Button):
+class BackButton(pygame.sprite.Sprite):
     def __init__(
-        self, groups, pos, font, function, size: tuple[float, float] = (300, 100)
+        self, groups, pos, function, size: tuple[float, float] = (300, 100)
     ) -> None:
-        super().__init__(groups, pos, "R", font, function, size)
+        super().__init__(groups)
+
+        self.pos = pos
+        self.function = function
+        self.size = size
+
+        self.normal_surf = self.create_normal_surf()
+        # self.normal_surf = self.create_clicked_surf()
+        self.hover_surf = self.create_hover_surf()
+        self.clicked_surf = self.create_clicked_surf()
+
+        self.hover = False
+
+        self.image = self.normal_surf
+        self.rect: pygame.FRect = self.image.get_frect(center=pos)  # type: ignore
+        self.normal_rect: pygame.FRect = self.normal_surf.get_frect(center=pos)
 
     def create_normal_surf(self) -> pygame.Surface:
         # surf = pygame.Surface(
@@ -236,12 +251,67 @@ class BackButton(Button):
         # ).convert_alpha()
         # text_render =
         # surf.blit(text_render, text_render.get_frect(center=(self.size[0]/2, self.size[1]/2)))
-        return self.font.render(self.text, True, "black")
+        surf = pygame.Surface(
+            self.size, flags=pygame.SRCALPHA, depth=32
+        ).convert_alpha()
+        pygame.draw.polygon(
+            surface=surf,
+            color="black",
+            points=[
+                (0, self.size[1] / 2),
+                (self.size[0], 0),
+                (self.size[0], self.size[1])
+            ] # type: ignore
+        )
+        return surf
 
     def create_hover_surf(self) -> pygame.Surface:
-        return self.font.render(self.text, True, "white")
+        surf = pygame.Surface(
+            self.size, flags=pygame.SRCALPHA, depth=32
+        ).convert_alpha()
+        pygame.draw.polygon(
+            surface=surf,
+            color="white",
+            points=[
+                (0, self.size[1] / 2),
+                (self.size[0], 0),
+                (self.size[0], self.size[1])
+            ] # type: ignore
+        )
+        return surf
 
-    # def create_clicked_surf(self) -> pygame.Surface:
-    #     return pygame.transform.scale_by(
-    #         self.font.render(self.text, True, "white"), 0.8
-    #     )
+    def create_clicked_surf(self) -> pygame.Surface:
+        surf = pygame.Surface(
+            self.size, flags=pygame.SRCALPHA, depth=32
+        ).convert_alpha()
+        pygame.draw.polygon(
+            surface=surf,
+            color="white",
+            points=[
+                (self.size[0] * 0.2, self.size[1] / 2),
+                (self.size[0] * 0.8, self.size[1] * 0.2),
+                (self.size[0] * 0.8, self.size[1] * 0.8)
+            ] # type: ignore
+        )
+        return surf
+    
+    def update(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.isOver(mouse_pos):
+            self.hover = True
+            if pygame.mouse.get_pressed()[0]:
+                self.image = self.clicked_surf
+                self.do_function = True
+            else:
+                self.image = self.hover_surf
+                if self.do_function:
+                    self.do_function = False
+                    self.function()
+        else:
+            self.hover = False
+            self.image = self.normal_surf
+            self.do_function = False
+        self.rect = self.image.get_frect(center=self.pos)  # type: ignore
+
+    def isOver(self, pos):
+        return self.normal_rect.collidepoint(pos)
